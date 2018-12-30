35db1ae34ffbacaf1e9a81718f8362ff194d651e
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-12-30 01:13:39 -0500

    Project- MemeMe! added share button and control logic for text fields

--- MemeMe!/ViewController.swift
+++ MemeMe!/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        topText.isHidden = true
+        bottomText.isHidden = true
+        shareButton.isEnabled = false
+        
+        self.topText.delegate = self
+        self.bottomText.delegate = self
+        
     }
 
     @IBAction func PickImageAction(_ sender: UIBarButtonItem) {
@@ -26,11 +37,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func shareImageAction(_ sender: UIBarButtonItem) {
+        print("This is share image action")
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             imagePickerView.image = image
         }
         dismiss(animated: true, completion: nil)
+        readyToAddText()
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -39,6 +55,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    func readyToAddText() {
+        topText.isHidden = false
+        bottomText.isHidden = false
+        
+        topText.isUserInteractionEnabled = true
+        bottomText.isUserInteractionEnabled = true
+        
+        topText.textColor = UIColor.white
+        bottomText.textColor = UIColor.white
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        return newText.length <= 25
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        shareButton.isEnabled = true
+        return true;
     }
 }
 
