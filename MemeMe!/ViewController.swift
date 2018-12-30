746b703762596fc7006ebd6fbbf794331b07a795
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-12-30 17:11:35 -0500

    Project- MemeMe! polished the textfields to show hide placeholders and editing to text attributes

--- MemeMe!/ViewController.swift
+++ MemeMe!/ViewController.swift
@@ -16,11 +16,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
     
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.yellow,
+        NSAttributedString.Key.foregroundColor: UIColor.red,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: 5]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         topText.isHidden = true
         bottomText.isHidden = true
+        
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = memeTextAttributes
+        
         shareButton.isEnabled = false
         
         self.topText.delegate = self
@@ -64,13 +73,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         topText.isUserInteractionEnabled = true
         bottomText.isUserInteractionEnabled = true
         
-        topText.textColor = UIColor.white
-        bottomText.textColor = UIColor.white
+        topText.text = ""
+        bottomText.text = ""
+        
+        topText.backgroundColor = UIColor.lightText
+        bottomText.backgroundColor = UIColor.lightText
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         var newText = textField.text! as NSString
         newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
         
         return newText.length <= 25
     }
@@ -78,7 +91,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         shareButton.isEnabled = true
+        
+        if ((textField.text! as NSString).length == 0) {
+            textField.backgroundColor = UIColor.lightText
+        }
         return true;
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.backgroundColor = UIColor.clear
+        textField.textAlignment = NSTextAlignment.center
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if ((textField.text! as NSString).length == 0) {
+            textField.backgroundColor = UIColor.lightText
+        }
     }
 }
 
