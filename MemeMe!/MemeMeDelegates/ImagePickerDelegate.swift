5c1c853c8d2cd5d82607441cacb8ab273c2a38c2
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-12-29 22:31:29 -0500

    Project- MemeMe! photo picker from album is ready

---
+++ MemeMe!/MemeMeDelegates/ImagePickerDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ImagePickerDelegate.swift
+//  MemeMe!
+//
+//  Created by Jasmeet Singh on 2018-12-29.
+//  Copyright © 2018 Jusmyth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ImagePickerDelegate:NSObject, UIImagePickerControllerDelegate {
+  
+}
--- MemeMe!/ViewController.swift
+++ MemeMe!/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func PickImageAction(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
